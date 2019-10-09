0ab4a49a5a0a00fe8cd01d2670953d393864fe09
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2019-10-09 19:03:34 +0530

    splash + working nav

--- app/src/main/java/com/syncx/a51ke/MainActivity.java
+++ app/src/main/java/com/syncx/a51ke/MainActivity.java
@@ -1,62 +1,50 @@
 package com.syncx.a51ke;
 
-import android.content.ClipData;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-        implements BottomNavigationView.OnNavigationItemSelectedListener {
-
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        navView.setOnNavigationItemSelectedListener(this);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
 
-        LoadFragment(new HomeFragment());
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
     }
 
-    private boolean LoadFragment(Fragment fragment){
-        if (fragment != null){
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment = null;
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
 
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, fragment);
+                case R.id.navigation_announcement:
+                    fragment = new AnnouncementFragment();
+                    break;
 
+                case R.id.navigation_pay:
+                    fragment = new PayFragment();
+                    break;
+
+                case R.id.navigation_profile:
+                    fragment = new ProfileFragment();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
             return true;
         }
-        return false;
-
-    }
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-        Fragment fragment = null;
-        switch (menuItem.getItemId()){
-            case R.id.navigation_home:
-                fragment = new HomeFragment();
-                break;
-
-            case R.id.navigation_announcement:
-                fragment = new AnnouncementFragment();
-                break;
-
-            case  R.id.navigation_pay:
-                fragment = new PayFragment();
-                break;
-
-            case R.id.navigation_profile:
-                fragment = new ProfileFragment();
-                break;
-        }
-        return LoadFragment(fragment);
-    }
+    };
 }
+
---
+++ app/src/main/java/com/syncx/a51ke/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.syncx.a51ke;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+}
